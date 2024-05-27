ec86777b030d07d96d361e56a0e17ec49b28fa18
Yadla Syam Sundar <115862918+Shyam3515@users.noreply.github.com>
2024-05-27 19:47:33 +0530

    Add files via upload

---
+++ Promises+API.js
@@ -0,0 +1,15 @@
+/**
+ * 1. What are Promises?
+Promises are objects which are used to perform asynchronous operations. They are just like placeholders to store a future value that will be returned after some time. They contain two properties: PromiseState and PromiseResult.
+
+1. Before promise we used to depend on callback functions which would result in 1.) Callback Hell (Pyramid of doom) | 2.) Inversion of control
+2. Inversion of control is overcome by using promise.
+  2.1) A promise is an object that represents eventual completion/failure of an asynchronous operation.
+  2.2) A promise has 3 states: pending | fulfilled | rejected.
+  2.3)  As soon as promise is fulfilled/rejected => It updates the empty object which is assigned undefined in pending state.
+  2.4) A promise resolves only once and it is immutable. 
+  2.5) Using .then() we can control when we call the cb(callback) function.
+
+3. To avoid callback hell (Pyramid of doom) => We use promise chaining. This way our code expands vertically instead of horizontally. Chaining is done using '.then()'
+4. A very common mistake that developers do is not returning a value during chaining of promises. Always remember to return a value. This returned value will be used by the next .then()
+ */
